1c925d2d11d5e9ea85e7a771e78490a300760b68
Ingenuity07 <shivamsi103@gmail.com>
2022-05-18 03:02:06 +0530

    init changes

--- src/index.js
+++ src/index.js
@@ -30,17 +30,17 @@ app.use('/document',document);
 
 app.use(express)
 
-__dirname=path.resolve()
+// __dirname=path.resolve()
 
   
-if(process.env.NODE_ENV == "production")
-{
-    app.use(express.static(path.join(__dirname,'/client/build')))
+// if(process.env.NODE_ENV == "production")
+// {
+//     app.use(express.static(path.join(__dirname,'/client/build')))
 
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//     })
+// }
 
 
 
